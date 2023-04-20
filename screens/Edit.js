36b505a43fa96dbd83ab8a8a5406ec59c418e555
Jing Li <jl.jing02123@gmail.com>
2023-04-20 05:00:35 +0000

    Apr 20, 2023, 1:00 AM

--- screens/Edit.js
+++ screens/Edit.js
@@ -1,11 +1,11 @@
 import { StyleSheet, TextInput, View } from "react-native";
-import { Button, Text } from "@rneui/themed";
+import { Button, Text, CheckBox } from "@rneui/themed";
 import * as Font from "expo-font";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import React, { useState, useCallback, useEffect, useMemo } from "react";
 import { StackActions } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ButtonGroup, CheckBox } from "react-native-elements";
+import { ButtonGroup } from "react-native-elements";
 
 async function cacheFonts(fonts) {
   return fonts.map(async (font) => await Font.loadAsync(font));
