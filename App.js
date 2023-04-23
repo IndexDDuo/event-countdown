2d11f4a82b41babdf42c17e15cb0c76ffb7a532a
Jing Li <jl.jing02123@gmail.com>
2023-04-23 10:13:15 +0000

    Apr 23, 2023, 6:13 AM

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import React, { useState, useCallback, useEffect, useMemo } from "react";
 import Home from "./screens/Home.js";
 import Detail from "./screens/Detail.js";
 import Edit from "./screens/Edit.js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
