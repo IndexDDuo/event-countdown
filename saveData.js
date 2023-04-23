75db5060d98b0268a7421151098992a569e20b55
Jing Li <jl.jing02123@gmail.com>
2023-04-23 06:02:12 +0000

    Apr 23, 2023, 2:02 AM

--- saveData.js
+++ saveData.js
@@ -1,30 +1,30 @@
-// //https://github.com/itinance/react-native-fs#readfilefilepath-string-encoding-string-promisestring
-// //react native fs
-// //npm install react-native-fs
-import RNFS from "react-native-fs";
+// // //https://github.com/itinance/react-native-fs#readfilefilepath-string-encoding-string-promisestring
+// // //react native fs
+// // //npm install react-native-fs
+// import RNFS from "react-native-fs";
 
-// // export function readFile(
-// // 	filepath: string,
-// // 	encodingOrOptions?: any
-// // ): Promise<string>
+// // // export function readFile(
+// // // 	filepath: string,
+// // // 	encodingOrOptions?: any
+// // // ): Promise<string>
 
-// // export function writeFile(
-// // 	filepath: string,
-// // 	contents: string,
-// // 	encodingOrOptions?: any
-// // ): Promise<void>
+// // // export function writeFile(
+// // // 	filepath: string,
+// // // 	contents: string,
+// // // 	encodingOrOptions?: any
+// // // ): Promise<void>
 
-// // export function appendFile(
-// // 	filepath: string,
-// // 	contents: string,
-// // 	encodingOrOptions?: string
-// // ): Promise<void>
+// // // export function appendFile(
+// // // 	filepath: string,
+// // // 	contents: string,
+// // // 	encodingOrOptions?: string
+// // // ): Promise<void>
 
-export const readFile = async (path) => {
-  try {
-    const content = await RNFS.readFile(path, "utf8");
-    return JSON.parse(content);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// export const readFile = async (path) => {
+//   try {
+//     const content = await RNFS.readFile(path, "utf8");
+//     return JSON.parse(content);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
