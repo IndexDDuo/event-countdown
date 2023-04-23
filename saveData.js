804f4e6edd335ad34c193e18e507e11fc2b79557
Jing Li <jl.jing02123@gmail.com>
2023-04-23 05:06:07 +0000

    Apr 23, 2023, 1:06 AM

---
+++ saveData.js
@@ -0,0 +1,4 @@
+//https://github.com/itinance/react-native-fs#readfilefilepath-string-encoding-string-promisestring
+//react native fs
+//npm install react-native-fs
+import RNFS from "react-native-fs";
