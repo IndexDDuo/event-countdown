1ee821aaf169be550fa294129195f76cbddc057b
Jing Li <jl.jing02123@gmail.com>
2023-04-24 06:01:44 +0000

    Apr 24, 2023, 2:01 AM

--- screens/Home.js
+++ screens/Home.js
@@ -54,7 +54,7 @@ export default function Home({ navigation, route }) {
       }
     };
     logAllItems();
-  }, []);
+  }, [modalVisible]);
 
   function calcDays(date) {
     const oneDay = 24 * 60 * 60 * 1000;
