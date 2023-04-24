ea656491d04840b0db2a0a71ef68e2f95160deb7
Jing Li <jl.jing02123@gmail.com>
2023-04-24 04:02:57 +0000

    Apr 24, 2023, 12:02 AM

--- screens/Home.js
+++ screens/Home.js
@@ -80,7 +80,7 @@ export default function Home({ navigation }) {
 
   const handlePress = (navigation) => {
     setModalVisible(true);
-    navigation.navigate("Home", { modalVisible: true });
+    // navigation.navigate("Home", { modalVisible: true });
   };
 
   const TaskDisplay = ({ name, date }) => (
@@ -164,5 +164,46 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 5,
     marginTop: 2,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
