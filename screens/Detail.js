4079c5dca335b3dbed889dfe48273fa1c28cd5cd
Jing Li <jl.jing02123@gmail.com>
2023-04-24 06:29:00 +0000

    Apr 24, 2023, 2:29 AM

--- screens/Detail.js
+++ screens/Detail.js
@@ -17,6 +17,20 @@ export default function Detail({ navigation, route }) {
   const { name, date, desc } = route.params;
   console.log(name + date + desc);
 
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">Too late...</div>;
+    }
+
+    return (
+      <div className="timer">
+        <div className="text">Remaining</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">seconds</div>
+      </div>
+    );
+  };
+
   return (
     <SafeAreaView>
       <View>
