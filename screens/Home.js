e53626075ea7179387c1de89d4fb0e43cbbe4fcf
Jing Li <jl.jing02123@gmail.com>
2023-04-24 07:18:41 +0000

    Apr 24, 2023, 3:18 AM

--- screens/Home.js
+++ screens/Home.js
@@ -46,7 +46,13 @@ export default function Home({ navigation, route }) {
         }
         // console.log("items after loop" + event2);
         // console.log(JSON.parse(items[0][1]).eventName);
-        event2.sort((a, b) => new Date(b.eventDate) - new Date(a.eventDate));
+        event2.sort((a, b) => {
+          const dateA = new Date(a.eventDate);
+          const dateB = new Date(b.eventDate);
+          const now = Date.now();
+          if(dateB>)
+          return new Date(b.eventDate) - new Date(a.eventDate);
+        });
         setEvents(event2);
         // console.log("items after loop" + event2[0].eventName);
       } catch (e) {
